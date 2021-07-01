cbbfee68d0aa919b61ceaeeb358ebad0655b5207
hokor <hokoro123@gmail.com>
2021-07-01 17:19:25 +0900

    copy file cheon1

---
+++ accountsappcopy/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path, include
+
+from accountsappcopy.views import hello_world
+
+app_name ='accountscopyapp'
+urlpatterns = [
+    path('hello_world/',hello_world,name = 'hello_world')
+    #이떄 url path 를 만들어도 accountsappcopy app에는 url이 없다
+]
