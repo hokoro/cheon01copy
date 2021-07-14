dee770b46ee0d37fefccd970d2b45a28db0c1ead
hokor <hokoro123@gmail.com>
2021-07-14 12:13:39 +0900

    7-14 copycommit2

--- accountsappcopy/urls.py
+++ accountsappcopy/urls.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path, include
 
 from accountsappcopy.views import hello_world, AccountCreateView
@@ -6,6 +7,8 @@ from accountsappcopy.views import hello_world, AccountCreateView
 app_name ='accountsapp'
 urlpatterns = [
     path('hello_world/',hello_world,name = 'hello_world'),
+    path('login/',LoginView.as_view(template_name = 'accountsapp/login.html'),name = 'login'),
+    path('logout/',LogoutView.as_view(),name = 'logout'),
     #이떄 url path 를 만들어도 accountsappcopy app에는 url이 없다
     path('create/',AccountCreateView.as_view(),name='create')
 ]
