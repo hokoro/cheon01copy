437a17b4b6c4fde67dc5c7661530f13aa4308414
hokoro <hokoro123@gmail.com>
2021-08-12 17:58:09 +0900

    8-12 commit 5

--- projectapp/urls.py
+++ projectapp/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 
-urlpatterns = [
+from projectapp.views import ProjectCreateView
 
+app_name = 'projectapp'
+urlpatterns = [
+    path('create/',ProjectCreateView.as_view(),name ='create'),
 ]
