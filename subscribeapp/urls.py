74779abd72bea7d127f1c428b032a02521c2fd33
hokoro <hokoro123@gmail.com>
2021-08-19 18:35:34 +0900

    8-19 commit 6

--- subscribeapp/urls.py
+++ subscribeapp/urls.py
@@ -1,2 +1,8 @@
+from django.urls import path
+
+from subscribeapp.views import SubscriptionView
+
 app_name = 'subscribeapp'
-urlpatterns = []
+urlpatterns = [
+    path('subscription/<int:project_pk>',SubscriptionView.as_view(),name='subscribe'),
+]
--- subscribeapp/views.py
+++ subscribeapp/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse
+from django.views.generic import RedirectView
+
+from projectapp.models import Project
+from subscribeapp.models import Subscription
+
+
+class SubscriptionView(RedirectView):
+    def get(self, request, *args, **kwargs): #게시판 정보와 user 정보
+        user = request.user
+        project= Project.objects.get(pk=kwargs['project_pk'])
+
+        subscription = Subscription.objects.filter(user=user,project=project)
+
+        #구독자 객체가 존재 하면
+        if subscription.exists():
+            subscription.delete()
+        else:
+            Subscription(user=user,project=project).save()
+
+        return super().get(request, *args, **kwargs)
+
+    def get_redirect_url(self, *args, **kwargs):
+        return reverse('projectapp:detail',kwargs={'pk':kwargs['project_pk']})
+
