6c36e29b9088ef913b59f512efd804a8b424fc2e
hokoro <hokoro123@gmail.com>
2021-09-23 17:48:43 +0900

    09-23 commit 3

--- cheon1copy/settings/deploy.py
+++ cheon1copy/settings/deploy.py
@@ -1,23 +1,16 @@
 from .base import *
 
-env_list = dict() #secret key 를 담은 dictionary 생성
-
-local_env = open(os.path.join(BASE_DIR,".env"))
-
-while True:
-    line = local_env.readline()
-    if not line:
-        break
-    line = line.replace('\n','')
-    start = line.find('=')
-    key = line[:start]
-    value = line[start+1:]
-    env_list[key] = value
+def read_secret(secret_name):
+    file = open('/run/secrets/'+secret_name) #암호키가 있는 경로를 설정
+    secret = file.read() #read 함수를 통해 파일 읽고
+    secret = secret.rstrip().lstrip() #좌우 공백들 지우기
+    file.close() #파일 닫기
+    return secret
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env_list['SECRET_KEY']
+SECRET_KEY = read_secret('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -29,12 +22,13 @@ ALLOWED_HOSTS = ['*']
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', #mysql 에서 분화 되어 나온게 mariadb 이다
         'NAME': 'django',
-        'USER': 'django',
-        'PASSWORD': 'fkaus132^^',
+        'USER': read_secret('MARIADB_USER'),
+        'PASSWORD': read_secret('MARIADB_PASSWORD'),
         'HOST': 'mariadb', #네트워크 를 사용할때 container 에서 mariadb 를 도메인처럼 사용하기 위해서  즉 container name 이 들어간다.
         'PORT': '3306',
     }
